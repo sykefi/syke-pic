cdcb778819a74b11b612f54c87201decbe1e2d4b
Otso Velhonoja <>
2020-11-09 22:53:21 +0200

    Allow evaluation parsing with different thresholds

--- sykepic/analysis/evaluation.py
+++ sykepic/analysis/evaluation.py
@@ -1,28 +1,39 @@
 from collections import Counter
 from pathlib import Path
 
+import pandas as pd
 
-def parse_evaluations(in_files, out_file, empty='unclassifiable'):
-    if isinstance(in_files, (str, Path)):
-        in_files = Path(in_files)
-        if in_files.is_dir():
-            in_files = list(in_files.rglob('*.eval.csv'))
-        else:
-            in_files = [in_files]
+from .dataframe import read_predictions
+
+
+def parse_evaluations(evaluations, predictions, thresholds, out_file,
+                      empty='unclassifiable'):
+    eval_df, samples = read_evaluations(evaluations)
+    try:
+        predictions = [next(Path(predictions).rglob(f'{sample}.csv'))
+                       for sample in samples]
+    except StopIteration:
+        print('[ERROR] Cannot find prediction files.')
+        raise
+    pred_df = read_predictions(predictions, thresholds)
+
     class_results = {}
-    for file in in_files:
-        with open(file) as fh:
-            fh.readline()
-            for line in fh:
-                line = line.strip().split(',')
-                for name, result in classification_result(
-                        line[1], line[2], empty=empty):
-                    class_results.setdefault(
-                        name, {'tp': 0, 'fp': 0, 'fn': 0})[result] += 1
+    for idx, row in eval_df.iterrows():
+        prediction, confidence, threshold = pred_df.loc[idx,
+            ['prediction', 'confidence', 'threshold']]
+        if confidence < threshold:
+            prediction = empty
+        actual = row['actual']
+        for name, result in classification_result(prediction, actual, empty):
+            class_results.setdefault(
+                name, {'tp': 0, 'fp': 0, 'fn': 0})[result] += 1
     # Total TN = Empty TP
-    total = Counter({'tn': class_results[empty]['tp']})
-    # Empty class is not needed any more
-    del class_results[empty]
+    if empty in class_results:
+        total = Counter({'tn': class_results[empty]['tp']})
+        # Empty class is not needed anymore
+        del class_results[empty]
+    else:
+        total = Counter({'tn': 0})
     # Add class results to total
     for result in class_results.values():
         total.update(result)
@@ -38,7 +49,27 @@ def parse_evaluations(in_files, out_file, empty='unclassifiable'):
             fh.write(f'{name},{prec:.2f},{rec:.2f},{F1:.2f},{sup},\n')
 
 
-def classification_result(predicted, actual, empty='unclassifiable'):
+def read_evaluations(evaluations):
+    if isinstance(evaluations, (str, Path)):
+        evaluations = Path(evaluations)
+        if evaluations.is_dir():
+            evaluations = list(evaluations.rglob('*.select.csv'))
+        else:
+            evaluations = [evaluations]
+    df_list = []
+    samples = []
+    for file in evaluations[:1]:
+        sample = Path(file).with_suffix('').with_suffix('').name
+        samples.append(sample)
+        df = pd.read_csv(file, header=None, names=['roi', 'actual'])
+        df.insert(0, 'sample', sample)
+        df.set_index(['sample', 'roi'], inplace=True)
+        df_list.append(df)
+    df = pd.concat(df_list)
+    return df, samples
+
+
+def classification_result(predicted, actual, empty):
     if predicted == actual:
         # True positive (nice!)
         # Also True negatives are returned here, i.e. empty, empty
@@ -66,12 +97,18 @@ def classification_scores(tp, fp, fn, tn=None):
         precision = 0
         recall = 0
         F1 = 0
+    # Note that support might be bigger than the actual number of
+    # labeled ROIs. This is because a wrongly predicted class will produce
+    # two errors (fp and fn) which are added here.
+    # So the same ROI will contribute twice to the final sum.
+    # I don't think this is a bad thing, since both of these values
+    # affect the F1-score, and thus should be counted separately.
     support = tp + fp + fn
     if tn:
         specificity = tn / (tn + fp)
         support += tn
     else:
-        specificity = None
+        specificity = 0
     return (precision, recall, F1, support, specificity)
 
 
