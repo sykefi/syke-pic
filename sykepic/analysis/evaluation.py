9d30012de72014aba846cac132ad1e5c85233bc1
Otso Velhonoja <>
2020-11-12 22:35:03 +0200

    Improve evaluation score handling

--- sykepic/analysis/evaluation.py
+++ sykepic/analysis/evaluation.py
@@ -1,13 +1,15 @@
 from collections import Counter
 from pathlib import Path
 
+import numpy as np
 import pandas as pd
 
 from .dataframe import read_predictions
 
 
 def parse_evaluations(evaluations, predictions, thresholds, out_file,
-                      empty='unclassifiable'):
+                      empty='unclassifiable', threshold_search=False,
+                      search_precision=0.01):
     eval_df, samples = read_evaluations(evaluations)
     try:
         predictions = [next(Path(predictions).rglob(f'{sample}.csv'))
@@ -17,36 +19,9 @@ def parse_evaluations(evaluations, predictions, thresholds, out_file,
         raise
     pred_df = read_predictions(predictions, thresholds)
 
-    class_results = {}
-    for idx, row in eval_df.iterrows():
-        prediction, confidence, threshold = pred_df.loc[idx,
-            ['prediction', 'confidence', 'threshold']]
-        if confidence < threshold:
-            prediction = empty
-        actual = row['actual']
-        for name, result in classification_result(prediction, actual, empty):
-            class_results.setdefault(
-                name, {'tp': 0, 'fp': 0, 'fn': 0})[result] += 1
-    # Total TN = Empty TP
-    if empty in class_results:
-        total = Counter({'tn': class_results[empty]['tp']})
-        # Empty class is not needed anymore
-        del class_results[empty]
-    else:
-        total = Counter({'tn': 0})
-    # Add class results to total
-    for result in class_results.values():
-        total.update(result)
-    # Output scores to file
-    with open(out_file, 'w') as fh:
-        fh.write(f'class,precision,recall,f1-score,support,tn-rate\n')
-        prec, rec, F1, sup, spec = classification_scores(
-                total['tp'], total['fp'], total['fn'], tn=total['tn'])
-        fh.write(f'total,{prec:.2f},{rec:.2f},{F1:.2f},{sup},{spec:.2f}\n')
-        for name, values in sorted(class_results.items()):
-            prec, rec, F1, sup, spec = classification_scores(
-                values['tp'], values['fp'], values['fn'])
-            fh.write(f'{name},{prec:.2f},{rec:.2f},{F1:.2f},{sup},\n')
+    result_df = results_as_df(eval_df, pred_df, empty, threshold_search,
+                              np.arange(0, 1+search_precision, search_precision))
+    return result_df
 
 
 def read_evaluations(evaluations):
@@ -58,7 +33,7 @@ def read_evaluations(evaluations):
             evaluations = [evaluations]
     df_list = []
     samples = []
-    for file in evaluations[:1]:
+    for file in evaluations:
         sample = Path(file).with_suffix('').with_suffix('').name
         samples.append(sample)
         df = pd.read_csv(file, header=None, names=['roi', 'actual'])
@@ -67,6 +42,49 @@ def read_evaluations(evaluations):
         df_list.append(df)
     df = pd.concat(df_list)
     return df, samples
+
+
+def results_as_df(eval_df, pred_df, empty, threshold_search, search_range):
+    result_dict = {}
+    for idx, row in eval_df.iterrows():
+        prediction, confidence, threshold = pred_df.loc[idx, [
+            'prediction', 'confidence', 'threshold']]
+        actual = row['actual']
+        if threshold_search:
+            thresholds = search_range
+        else:
+            # Only one threshold per class, but iterate over it all the same
+            thresholds = [threshold]
+        for threshold in thresholds:
+            if confidence < threshold:
+                prediction = empty
+            for name, result in classification_result(prediction, actual, empty):
+                result_dict.setdefault(name, {})
+                result_dict[name].setdefault(threshold,
+                                             {'tp': 0, 'tn': 0,
+                                              'fp': 0, 'fn': 0})[result] += 1
+    # Shape result_dict to have multi-index
+    result_dict = {(name, thres): results
+                   for name, thres_results in result_dict.items()
+                   for thres, results in thres_results.items()}
+    result_df = pd.DataFrame.from_dict(
+        result_dict, orient='index').sort_index()
+    if not threshold_search:
+        # Drop threshold from multiindex
+        result_df.index = result_df.index.droplevel(1)
+        # Make row ('all') for joined results
+        if empty in result_df.index:
+            tn = result_df.loc['unclassifiable', 'tp'].sum()
+            result_df.drop(index=empty, inplace=True)
+            result_df.loc['all'] = [result_df.tp.sum(), tn,
+                                    result_df.fp.sum(), result_df.fn.sum()]
+    elif empty in result_df.index:
+        result_df.drop(index=empty, level=0, inplace=True)
+    score_df = result_df.apply(lambda row: classification_scores(
+                               row.tp, row.tn, row.fp, row.fn),
+                               axis=1, result_type='expand')
+    score_df.columns = ('precision', 'recall', 'F1', 'support', 'specificity')
+    return pd.concat((result_df, score_df), axis=1)
 
 
 def classification_result(predicted, actual, empty):
@@ -88,7 +106,7 @@ def classification_result(predicted, actual, empty):
         return ((predicted, 'fp'), (actual, 'fn'))
 
 
-def classification_scores(tp, fp, fn, tn=None):
+def classification_scores(tp, tn, fp, fn):
     if tp > 0:
         precision = tp / (tp + fp)
         recall = tp / (tp + fn)
