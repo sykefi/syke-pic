b4a769c0fd38130f7060a470ea99b3eb84c8099b
Otso Velhonoja <48589417+veot@users.noreply.github.com>
2021-07-22 10:24:38 +0300

    Fix upload suffix bug

--- sykepic/sync/process.py
+++ sykepic/sync/process.py
@@ -332,8 +332,7 @@ def upload(upload_record, compression, local_dir, bucket_dir, bucket, suffix=Non
             # Archive path: local_dir/yyyy/mm/yyyymmdd[.suffix].compression
             archive = local_dir / day_dir.parent / f"{''.join(day_dir.parts[-3:])}"
             if suffix:
-                suffix += f".{compression}"
-                archive = archive.with_suffix(suffix)
+                archive = archive.with_suffix(f"{suffix}.{compression}")
             else:
                 archive = archive.with_suffix(f".{compression}")
             create_archive(local_dir / day_dir, archive, compression)
